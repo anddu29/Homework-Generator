ef7c96bed8e1ecd8ea9ac68b71b8400643d9b680
Andi <andrei.stefan.ok@gmail.com>
2022-04-14 14:46:06 +0300

    TODO: Rework Classes

---
+++ main.py
@@ -0,0 +1,15 @@
+import methods
+
+
+if __name__ == "__main__":
+    chapters = int(input("Introduce number of chapters:\n"))
+    print(f"There are {chapters} chapters.")
+
+
+    for x in range(1, chapters+1):
+        romanchapter = methods.printRoman(x)
+        problems = input(f"Introduce number of problems for chapter {romanchapter}:\n")
+        print(f"There are {problems} problems for Chapter {romanchapter}.")
+
+
+
---
+++ methods.py
@@ -0,0 +1,27 @@
+class methods:
+    def printRoman(num):
+        val = [
+                1000, 900, 500, 400,
+                100, 90, 50, 40,
+                10, 9, 5, 4,
+                1
+                ]
+        syb = [
+                "M", "CM", "D", "CD",
+                "C", "XC", "L", "XL",
+                "X", "IX", "V", "IV",
+                "I"
+                ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+class books:
+    def __init__(self, chapters):
+        self.chapters = 
+        self.age = age
