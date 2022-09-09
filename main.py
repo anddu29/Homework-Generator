9117a24f44b1255f94664ca31398a6223bd01a74
anddu29 <76963775+anddu29@users.noreply.github.com>
2022-09-09 11:45:48 +0300

    Submit prod version
    Homework Generator where it picks random exercises from a random chapter of the book initialized by the user. User requests a number of problems for the homework and the script responses with an array with the problems from a random chapter.

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
+__author__ = "Andrei Stefan"
+__version__ = "1.0.0"
+__email__ = "andistef29@gmail.com"
+__status__ = "Production"
+
 import methods
 
-
-
 if __name__ == "__main__":
+    while True:
+        try:
+            bookNumberChapters = int(input("Enter the number of chapters for your book: "))
+        except ValueError:
+            print("Incorrect value type for number of chapters! (Expected integer)")
+            continue
+        break
+    book = methods.book(bookNumberChapters)
 
-    MathBook = methods.book(5)
-    MathBook.display_chapters()
-    MathBook.register_problems(1,5)
+    for i in range(bookNumberChapters):
+        while True:
+            try:
+                bookNumberProblems = int(input(f"Enter the number of chapters for chapter number {i + 1}: "))
+                book.register_problems(i + 1, bookNumberProblems)
+            except ValueError:
+                print("Incorrect value type for number of problems per chapter! (Expected integer)")
+                continue
+            break
 
-    MathBook.display_chapter_problems(1)
-    MathBook.display_chapter_problems(2)
-    MathBook.display_chapter_problems(6)
+    while True:
+        try:
+            homeworkStudentName = input("Enter the name of the student you want to assign the homework to: ")
+        except ValueError:
+            print("Incorrect value type for name! (Expected string)")
+            continue
+        break
 
-    
-    
+    while True:
+        try:
+            homeworkNumberProblems = int(input("Enter the number of problems for homework: "))
+        except ValueError:
+            print("Incorrect value type for name! (Expected string)")
+            continue
+        break
 
+    book.random_generator(homeworkStudentName, homeworkNumberProblems)
+
+    print("\nGeneral information about the book: ")
+    for i in range(1, bookNumberChapters):
+        book.display_chapter_problems(i)
--- methods.py
+++ methods.py
@@ -1,44 +1,59 @@
-from pkg_resources import working_set
+__author__ = "Andrei Stefan"
+__version__ = "1.0.0"
+__email__ = "andistef29@gmail.com"
+__status__ = "Production"
 
-def printRoman(num):
-        val = [
-                1000, 900, 500, 400,
-                100, 90, 50, 40,
-                10, 9, 5, 4,
-                1
-                ]
-        syb = [
-                "M", "CM", "D", "CD",
-                "C", "XC", "L", "XL",
-                "X", "IX", "V", "IV",
-                "I"
-                ]
-        roman_num = ''
-        i = 0
-        while  num > 0:
-            for _ in range(num // val[i]):
-                roman_num += syb[i]
-                num -= val[i]
-            i += 1
-        return roman_num
+import random
+
+
+def print_roman(num):
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    roman_num = ''
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syb[i]
+            num -= val[i]
+        i += 1
+    return roman_num
+
+
 class book:
     def __init__(self, chapters):
         self.chapters = chapters
         self.problems = dict()
 
-    def display_chapters (self):
-        
-        print (f"There are {self.chapters} chapters in this book.")
+    def display_chapters(self):
+        print(f"There are {self.chapters} chapters in this book.")
 
-    def register_problems (self,chapter_number,number_problems):
-        if int(chapter_number) <= (int(self.chapters)):
-            self.problems[chapter_number] = number_problems
+    def register_problems(self, chapterNumber, numberProblems):
+        if int(chapterNumber) <= (int(self.chapters)):
+            self.problems[chapterNumber] = numberProblems
 
-    def display_chapter_problems (self,chapter_number):
-        if chapter_number > self.chapters:
-            print ("The number of chapters are not yet defined.")
-        elif chapter_number not in self.problems:
+    def display_chapter_problems(self, chapterNumber):
+        if chapterNumber > self.chapters:
+            print("The number of chapters are not yet defined.")
+        elif chapterNumber not in self.problems:
             print("The number of problems for this chapter was not defined yet.")
         else:
-            print (f"There are {self.problems[chapter_number]} problems in chapter {printRoman(chapter_number)}.")    
-    
+            print(f"There are {self.problems[chapterNumber]} problems in chapter {print_roman(chapterNumber)}.")
+
+    def random_generator(self, studentName, homeworkNumberProblems):
+        print(f"\nHomework generated for student {studentName} is:")
+        while True:
+            randomChapter = random.randint(1, self.chapters)
+            if self.problems[randomChapter] > homeworkNumberProblems:
+                break
+        print(
+            f"From chapter {randomChapter}, following problems: {sorted(random.sample(range(1, self.problems[randomChapter]), homeworkNumberProblems))}")
